1654563965cdb0dd451fb981d71d710a72b987f4
zheldybin <zheldybin.aa@mail.ru>
2026-09-12 17:55:34 +0300

    homework part two

---
+++ homework_1/homework_1.7.py
@@ -0,0 +1,3 @@
+arr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+print(arr[1])
---
+++ homework_1/homework_1.8.py
@@ -0,0 +1,4 @@
+str_one = "employ"
+str_two = "employment"
+
+print(str_one in str_two)
